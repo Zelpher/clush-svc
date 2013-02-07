4052d8c8f4b280752a13019b75f3a5e92f3403b8
Alexandre Sicard <alex@libricoleur.fr>
2013-02-07 23:42:35 +0100

    Fixes indentation
    Franck's editor inserted tabs instead of spaces.

--- templates.py
+++ templates.py
@@ -1,26 +1,26 @@
 managers = {
-	'bsdinit': {
-		'base': '/etc/rc.d/',
-		'start': "%s start",
-		'stop': "%s stop",
-		'restart': "%s status",
-		'status': "%s status",
-	},
-	'systemd': {
-		'base': 'systemctl ',
-		'start': "%s.service start",
-		'stop': "%s.service stop",
-		'restart': "%s.service status",
-		'status': "%s.service status",
-	},
-	'sysvinit': {
-		'base': '/etc/init.d/',
-		'start': "%s start",
-		'stop': "%s stop",
-		'restart': "%s status",
-		'status': "%s status",
-	}
+    'bsdinit': {
+        'base': '/etc/rc.d/',
+        'start': "%s start",
+        'stop': "%s stop",
+        'restart': "%s status",
+        'status': "%s status",
+    },
+    'systemd': {
+        'base': 'systemctl ',
+        'start': "%s.service start",
+        'stop': "%s.service stop",
+        'restart': "%s.service status",
+        'status': "%s.service status",
+    },
+    'sysvinit': {
+        'base': '/etc/init.d/',
+        'start': "%s start",
+        'stop': "%s stop",
+        'restart': "%s status",
+        'status': "%s status",
+    }
 }
 
 def get_command(manager, service, action):
-	return managers[manager]['base']+managers[manager][action]%service
+    return managers[manager]['base']+managers[manager][action]%service
